1d6df50862b4cb4f355deb373404076046544291
Bijan Shafiei <bshafiei33@gmail.com>
2017-06-19 17:10:54 -0500

    initial commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'CbKT1LZjnLPOiBr91fgD3FuNM',
+  consumer_secret: 'M0ViAylTKShMoGMgFvsyHzn8hmDqbfUtp2sVcl9EQBoJspYUzu',
+  access_token_key: '876916369178152960-PQoSSV38KdVxYTyZPII0BR4kG1Z0cF6',
+  access_token_secret: 'AZAOlguD0CFk82TDkGAHq4kiY8wl36IItBIIbUi2nlPe5',
+}
---
+++ liri.js
@@ -0,0 +1,7 @@
+var keys = require('./keys.js');
+var inquirer = require('inquirer');
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
+var fs = require('fs');
+var userKeys;
