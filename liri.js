dac5590261bc421058ce34052de52a95b3bcac64
Bijan Shafiei <bshafiei33@gmail.com>
2017-06-20 13:50:14 -0500

    twitter function working

--- liri.js
+++ liri.js
@@ -1,7 +1,39 @@
 var keys = require('./keys.js');
-var inquirer = require('inquirer');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
 var fs = require('fs');
-var userKeys;
+
+var  choices = {
+
+	mytweets: function (){
+		console.log("my tweets");
+		var client = new Twitter(keys.twitterKeys);
+		var params = {screen_name: 'bshafieitest'};
+		client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  			if (!error) {
+    		console.log(tweets);
+ 			}
+		});
+	},
+
+	spotifyThisSong: function (){
+		
+	},
+
+	movieThis: function (){
+		
+	},
+
+	doWhatItSays: function (){
+		
+	},
+	
+}
+//Taking user input
+switch (process.argv[2]) {
+		case "my-tweets":
+			choices.mytweets();
+			break;
+		
+	}
