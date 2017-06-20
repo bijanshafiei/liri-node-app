44ae810bb6cd7d4a90e9932d7038348f4090171f
Bijan Shafiei <bshafiei33@gmail.com>
2017-06-20 14:06:18 -0500

    updated twitter function

--- liri.js
+++ liri.js
@@ -12,7 +12,12 @@ var  choices = {
 		var params = {screen_name: 'bshafieitest'};
 		client.get('statuses/user_timeline', params, function(error, tweets, response) {
   			if (!error) {
-    		console.log(tweets);
+				for (var i=0; i<tweets.length; i++){
+					console.log('-----------------');
+					console.log(tweets[i].created_at);
+					console.log(tweets[i].text);
+					console.log('-----------------');
+				}
  			}
 		});
 	},
