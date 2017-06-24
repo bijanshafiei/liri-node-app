b6ef1b5a7b61b4e8f2a29dc3cab313fc2a1b0c87
Bijan Shafiei <bshafiei33@gmail.com>
2017-06-24 10:09:07 -0500

    spotify function working

--- keys.js
+++ keys.js
@@ -6,3 +6,8 @@ exports.twitterKeys = {
   access_token_key: '876916369178152960-PQoSSV38KdVxYTyZPII0BR4kG1Z0cF6',
   access_token_secret: 'AZAOlguD0CFk82TDkGAHq4kiY8wl36IItBIIbUi2nlPe5',
 }
+
+exports.spotifyKeys = {
+  id: "35f0c0c721cb449881bbac4284a1a0a5",
+  secret: "ea369f21c5ff4d249a438e0d1c9e2318",
+}
--- liri.js
+++ liri.js
@@ -1,29 +1,47 @@
+//Client keys
 var keys = require('./keys.js');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
+var inquirer = require('inquirer');
 var request = require('request');
 var fs = require('fs');
+var input = process.argv[2];
+var searchTerm = process.argv[3]
 
-var  choices = {
-
+//Functions that grab data based on user input
+var  lookup = {
+	//Grabs last 20 tweets from test account
 	mytweets: function (){
-		console.log("my tweets");
+		// console.log("my tweets");
 		var client = new Twitter(keys.twitterKeys);
 		var params = {screen_name: 'bshafieitest'};
 		client.get('statuses/user_timeline', params, function(error, tweets, response) {
   			if (!error) {
 				for (var i=0; i<tweets.length; i++){
-					console.log('-----------------');
+					console.log("\n--------------------\n");
 					console.log(tweets[i].created_at);
 					console.log(tweets[i].text);
-					console.log('-----------------');
+					console.log("\n--------------------\n");
 				}
  			}
 		});
 	},
-
+	
 	spotifyThisSong: function (){
-		
+		var spotify = new Spotify(keys.spotifyKeys);
+		spotify.search({ type: 'track', query: searchTerm, limit: 1 }, function(err, data) {
+  			if (err) {
+    			return console.log('Error occurred: ' + err);
+  			} 
+			var artist = data.tracks.items[0].album.artists[0].name;
+			var songName = data.tracks.items[0].name;
+			var link = data.tracks.items[0].external_urls.spotify;
+			var album = data.tracks.items[0].album.name;
+			console.log("Song: " + songName);
+			console.log("By: " + artist);
+			console.log("URL: " + link);
+			console.log("Album: " + album);
+		});
 	},
 
 	movieThis: function (){
@@ -35,10 +53,22 @@ var  choices = {
 	},
 	
 }
-//Taking user input
+//Processing user input
 switch (process.argv[2]) {
 		case "my-tweets":
-			choices.mytweets();
+			lookup.mytweets();
 			break;
-		
+		case "spotify-this-song":
+			lookup.spotifyThisSong();
+			break;
+		// case "movie-this":
+		// 	var movieName = process.argv[3];
+		// 	lookup.movieThis(movieName);
+		// 	break;
+		// case "do-what-it-says":
+		// 	lookup.doWhatItSays();
+		// 	break;	
+		// default:
+		// console.log("Choose a valid option");
+		// 	break;
 	}
